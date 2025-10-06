1a4b7b6573c934c86c39ed88b263a9999f0522a6
Chuiko Egor <egorchuiko7@gmail.com>
2025-10-06 23:30:05 +0300

    add Hibernate utility for session management

---
+++ src/main/java/by/chaika19/hibernate/starter/util/HibertnateUtil.java
@@ -0,0 +1,13 @@
+package by.chaika19.hibernate.starter.util;
+
+import by.chaika19.hibernate.starter.converter.BirthdayConverter;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibertnateUtil {
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration().configure();
+        configuration.addAttributeConverter(new BirthdayConverter());
+        return configuration.buildSessionFactory();
+    }
+}
