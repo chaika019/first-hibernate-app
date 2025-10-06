a1f32ef55370a079cfa7bc5fcc2304146bef3636
Chuiko Egor <egorchuiko7@gmail.com>
2025-10-07 01:04:45 +0300

    The old name had a mistake

--- src/main/java/by/chaika19/hibernate/starter/util/HibernateUtil.java
+++ src/main/java/by/chaika19/hibernate/starter/util/HibernateUtil.java
@@ -4,7 +4,7 @@ import by.chaika19.hibernate.starter.converter.BirthdayConverter;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibertnateUtil {
+public class HibernateUtil {
     public static SessionFactory buildSessionFactory() {
         Configuration configuration = new Configuration().configure();
         configuration.addAttributeConverter(new BirthdayConverter());
