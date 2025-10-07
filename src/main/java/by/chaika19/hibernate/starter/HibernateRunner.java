43e333acd0ff45cfece898e22dcca3be5cc2f4ca
Chuiko Egor <egorchuiko7@gmail.com>
2025-10-07 21:33:37 +0300

    testing and little fixes

--- src/main/java/by/chaika19/hibernate/starter/HibernateRunner.java
+++ src/main/java/by/chaika19/hibernate/starter/HibernateRunner.java
@@ -1,40 +1,42 @@
 package by.chaika19.hibernate.starter;
 
 import by.chaika19.hibernate.starter.entity.BirthDate;
+import by.chaika19.hibernate.starter.entity.PersonalInfo;
 import by.chaika19.hibernate.starter.entity.Role;
 import by.chaika19.hibernate.starter.entity.User;
-import by.chaika19.hibernate.starter.util.HibertnateUtil;
+import by.chaika19.hibernate.starter.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
 import java.time.LocalDate;
 
+@Slf4j
 public class HibernateRunner {
+
     public static void main(String[] args) {
         User user = User.builder()
-                .username("a52@gmail.com")
-                .firstname("Ivan")
-                .lastname("Ivanovich")
-                .birthDate(new BirthDate(LocalDate.of(2005, 10, 22)))
+                .username("vovan228@gmail.com")
+                .personalInfo(PersonalInfo.builder()
+                        .firstname("Vovan")
+                        .lastname("Vovanovich")
+                        .birthDate(new BirthDate(LocalDate.of(2005, 10, 22)))
+                        .build())
                 .role(Role.ADMIN)
                 .build();
+        log.info("User object in transient state {} ", user);
 
-        try (SessionFactory sessionFactory = HibertnateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
-                session1.beginTransaction();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            try (Session session = sessionFactory.openSession()) {
+                session.beginTransaction();
 
-                session1.saveOrUpdate(user);
+                session.merge(user);
 
-                user.setFirstname("Pasha");
-                System.out.println(session1.isDirty());
-
-                session1.getTransaction().commit();
+                session.getTransaction().commit();
             }
-//            try (Session session2 = sessionFactory.openSession()) {
-//                session2.beginTransaction();
-//                session2.delete(user);
-//                session2.getTransaction().commit();
-//            }
+        } catch (Exception e) {
+            log.error("Exception occurred: ", e);
+            throw e;
         }
     }
 }
