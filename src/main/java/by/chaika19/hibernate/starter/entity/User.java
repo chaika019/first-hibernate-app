3c7dda3b6156aa008922122e0fde2acbdf312ae3
Chuiko Egor <egorchuiko7@gmail.com>
2025-10-09 20:22:12 +0300

    Change id from Integer to Long; Add company, profile and userChats relationships; Add proper toString and equals/hashCode exclusions;  Add table annotations and constraints"

--- src/main/java/by/chaika19/hibernate/starter/entity/User.java
+++ src/main/java/by/chaika19/hibernate/starter/entity/User.java
@@ -1,34 +1,42 @@
 package by.chaika19.hibernate.starter.entity;
 
-import by.chaika19.hibernate.starter.converter.BirthdayConverter;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString(exclude = {"company", "profile", "userChats"})
+@EqualsAndHashCode(of = "username")
 @Builder
 @Entity
 @Table(name = "users", schema = "public")
 public class User {
     @Id
-//    Автоматом
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-//    Для ручной настройки
-//    @GeneratedValue(generator = "user_gen", strategy = GenerationType.SEQUENCE)
-//    @SequenceGenerator(name = "user_gen", sequenceName = "users_id_seq", allocationSize = 1)
-    private Integer id;
+    @Column(nullable = false, unique = true)
     private String username;
-//    Можно так
-//    @AttributeOverride(name = "birthDay", column = @Column(name = "birth_day"))
+
     @Embedded
     private PersonalInfo personalInfo;
+
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Profile profile;
+
+    @Builder.Default
+    @OneToMany(mappedBy = "user")
+    private List<UserChat> userChats = new ArrayList<>();
+
 }
